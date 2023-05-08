d960dd236b02790dda7348603164e50de71e19f2
Marut Prasad <marut.prasad@philips.com>
2023-05-08 12:15:06 +0530

    Adding basic checks

--- src/action.js
+++ src/action.js
@@ -1,5 +1,14 @@
 async function run(){
     console.log('Hello world');
+    var isPyAvailable = require('hasbin').sync('python');
+    console.log("Python installed : " + isPyAvailable);
+
+    const checkDiskSpace = require('check-disk-space').default
+    checkDiskSpace('C:/').then((diskSpace) => {
+        console.log(diskSpace)
+    })
+
+    console.log("Current directory", __dirname);
 }
 
 run();
